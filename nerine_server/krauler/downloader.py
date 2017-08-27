dc352482f938113fd3b18be246c5400d634f5938
kheeva <oleg.ivliev@mail.ru>
2017-08-28 01:16:52 +0300

    detection of file-type and charset was added.

--- nerine_server/krauler/downloader.py
+++ nerine_server/krauler/downloader.py
@@ -4,11 +4,12 @@
 
 import os
 import threading
+import re
 import urllib.request
 from config import db_host, db_port, db_username, db_password, db_name, db_charset
 import NerineDB
 from Logger import write_log
-from Parser import parse_file
+from Parser import parse_robots, parse_xml, parse_html
 
 
 mydb = NerineDB.NerineDb(
@@ -30,30 +31,67 @@ def add_robots():
             print(r[0])
 
 
+def get_file(url, file):
+    try:
+        urllib.request.urlretrieve(url, file)
+    except Exception as e:
+        print(e)
+    else:
+        print('got', url)
+
+
+def get_html_file(url, file):
+    req = urllib.request.urlopen(url)
+    try:
+        charset = req.info().get_content_charset()
+    except Exception as e:
+        print(e)
+        content = req.read().decode('utf-8')
+    else:
+        content = req.read().decode(charset)
+        print('charset =', charset)
+    finally:
+        with open(file, 'w', encoding='utf-8') as f:
+            f.write(content)
+        print('got', url)
+
 def download_html():
     new_pages =  mydb.get_pages_without_scan
 
     for url in new_pages[0]:
-        current_site_id = url[2]
+        current_url = url[0]
+        site_id = url[2]
         page_id = url[3]
-        current_url = url[0].replace('http://', '').replace('/', '_')
-        try:
-            current_dir = os.path.join('html', str(url[1]))
+        file = url[0].replace('http://', '').replace('/', '_')
+        current_dir = os.path.join('html', str(url[1]))
+        if not os.path.exists(current_dir):
+            os.makedirs(current_dir)
 
-            if not os.path.exists(current_dir):
-                os.makedirs(current_dir)
+        file = os.path.join(current_dir, file)
 
-            current_html_file = os.path.join(current_dir, current_url)
-            urllib.request.urlretrieve(url[0], current_html_file)
-        except Exception as e:
-            print('error with url ', url[0])
-            print(e)
+        if re.search(r'[^\s]+robots\.txt\b', current_url):
+            get_file(current_url, file)
+            parse_robots(file, site_id, mydb)
+        elif re.search(r'[^\s]+\.xml\b', current_url):
+            get_file(current_url, file)
+            parse_xml(file, site_id, mydb)
         else:
-            print('Downloaded ', url[0])
-            parse_file(mydb, current_html_file, current_site_id, page_id)
+            get_html_file(current_url, file)
+            parse_html(file, site_id, page_id, mydb, persons_dictionary, seek_words)
+
+        #mydb.set_pages_scantime(page_id)
 
 
 def main():
+    global persons_dictionary
+    global seek_words
+
+    persons_dictionary = mydb.get_persons[0]
+    seek_words = []
+
+    for pair in persons_dictionary.items():
+        seek_words.extend(pair[1])
+
     add_robots()
     download_html()
 
