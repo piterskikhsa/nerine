4ad25305f677a8a62a129cfe6626349723904703
kheeva <kheeva@ya.ru>
2017-08-28 21:10:50 +0300

    добавил проверку типа контента в get_html_file. добавил парсинг кириллических параметров в GET запросах. добавил проверки в get_file и get_html_file. не вылетаем при 404 и других ошибках.

--- nerine_server/krauler/downloader.py
+++ nerine_server/krauler/downloader.py
@@ -2,14 +2,13 @@
 # coding: utf-8
 
 
-import os
 import threading
 import re
 import urllib.request
 from config import db_host, db_port, db_username, db_password, db_name, db_charset
 import NerineDB
 from Logger import write_log
-from Parser import parse_robots, parse_xml, parse_html
+from Parser import parse_robots, parse_xml, parse_html, make_path_to_file, parse_url
 
 
 mydb = NerineDB.NerineDb(
@@ -34,30 +33,48 @@ def add_robots():
 def get_file(url, file):
     try:
         urllib.request.urlretrieve(url, file)
-    except Exception as e:
-        print(e)
+    except Exception as error:
+        print(error)
+        return False
     else:
         print('got', url)
+        return True
 
 
 def get_html_file(url, file):
+    print('getting NOT parsed url..', url)
+    if '?' and '=' in url:
+        url = parse_url(url)
     try:
         req = urllib.request.urlopen(url)
     except Exception as error:
         print(error)
+        return False
     else:
         try:
-            charset = req.info().get_content_charset()
+            content_type = req.info().get_content_subtype()
         except Exception as error:
             print(error)
-            content = req.read().decode('utf-8')
+            return False
         else:
-            content = req.read().decode(charset)
-            print('charset =', charset)
-        finally:
-            with open(file, 'w', encoding='utf-8') as f:
-                f.write(content)
-            print('got', url)
+            if re.search(r'[html|HTML]', content_type):
+                try:
+                    charset = req.info().get_content_charset()
+                except Exception as error:
+                    print(error)
+                    content = req.read().decode('utf-8')
+                else:
+                    content = req.read().decode(charset)
+                    print('charset =', charset)
+                finally:
+                    with open(file, 'w', encoding='utf-8') as f:
+                        f.write(content)
+                    print('got', url)
+
+                return True
+            else:
+                print(url, 'bad content-type:', content_type)
+                return False
 
 
 def download_html():
@@ -65,24 +82,23 @@ def download_html():
 
     for url in new_pages[0]:
         current_url = url[0]
+        site_name = url[1]
         site_id = url[2]
         page_id = url[3]
-        file = current_url.replace('http://', '').replace('/', '_')
-        current_dir = os.path.join('html', str(url[1]))
-        if not os.path.exists(current_dir):
-            os.makedirs(current_dir)
 
-        file = os.path.join(current_dir, file)
+        path_to_file = make_path_to_file(current_url, site_name)
+        print('path to file in downloader', path_to_file)
 
         if re.search(r'[^\s]+robots\.txt\b', current_url):
-            get_file(current_url, file)
-            parse_robots(file, site_id, mydb)
+            if get_file(current_url, path_to_file):
+                parse_robots(path_to_file, site_id, mydb)
         elif re.search(r'[^\s]+\.xml\b', current_url):
-            get_file(current_url, file)
-            parse_xml(file, site_id, mydb)
+            if get_file(current_url, path_to_file):
+                parse_xml(path_to_file, site_id, mydb)
         else:
-            get_html_file(current_url, file)
-            parse_html(file, site_id, page_id, mydb, persons_dictionary, seek_words)
+            if get_html_file(current_url, path_to_file):
+                parse_html(path_to_file, site_id, page_id, mydb,
+                           persons_dictionary, seek_words)
 
         mydb.set_pages_scantime(page_id)
 
