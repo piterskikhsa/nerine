ab22c765d87989f5987c24c61cafc202beaad2e1
kheeva <kheeva@ya.ru>
2017-08-30 19:41:12 +0300

    убрал принты, перенес все в логгинг

--- nerine_server/krauler/downloader.py
+++ nerine_server/krauler/downloader.py
@@ -2,13 +2,12 @@
 # coding: utf-8
 
 
-import threading
 import re
 import urllib.request
 import gzip
 from config import db_host, db_port, db_username, db_password, db_name, db_charset
 import NerineDB
-from Logger import write_log
+import  Logger
 from Parser import parse_robots, parse_xml_sitemap, parse_html, make_path_to_file,\
     parse_url, parse_txt_sitemap
 
@@ -19,64 +18,73 @@ mydb = NerineDB.NerineDb(
 
 def add_robots():
     new_sites = mydb.get_sites_without_pages
-    thread_1 = threading.Thread(target=write_log(new_sites))
-    thread_1.start()
-
     for i in new_sites[0]:
         r = mydb.insert_pages_robots(i[1], i[0])
-        thread_2 = threading.Thread(target=write_log(r))
-        thread_2.start()
         if r[0]:
-            print('OK')
+            Logger.logger.info('new robots page inserted.')
+            #print('new robots page inserted.')
         else:
-            print(r[0])
+            #print(r[0])
+            Logger.logger.error('could not insert robots.txt for %s', i[1])
 
 
 def get_file(url, file):
     try:
+        Logger.logger.info('getting a file... %s', url)
         urllib.request.urlretrieve(url, file)
     except Exception as error:
-        print(error)
+        #print(error)
+        Logger.logger.error('could not get: %s. output: %s', url, error)
         return False
     else:
-        print('got', url)
+        #print('got', url)
+        Logger.logger.info('GOT: %s. PUT it to %s', url, file)
         return True
 
 
 def get_html_file(url, file_path):
-    print('getting NOT parsed url..', url)
-    print('file path is', file_path)
+    #print('getting NOT parsed url..', url)
+    #print('file path is', file_path)
+    Logger.logger.info('getting a html file %s:', url)
     if '?' and '=' in url:
         url = parse_url(url)
     try:
+        Logger.logger.info('found http GET request. parsing params..')
         req = urllib.request.urlopen(url)
     except Exception as error:
-        print(error)
+        #print(error)
+        Logger.logger.error('could not get %s: output:', url, error)
         return False
     else:
         try:
+            Logger.logger.info('checking content-type..')
             content_type = req.info().get_content_subtype()
         except Exception as error:
-            print(error)
+            #print(error)
+            Logger.logger.error('could not get %s', url)
             return False
         else:
             if re.search(r'[html|HTML]', content_type):
                 try:
                     charset = req.info().get_content_charset()
                 except Exception as error:
-                    print(error)
+                    #print(error)
+                    Logger.logger.error('could not determine charset. %s . going to use utf-8..', error)
                     content = req.read().decode('utf-8')
                 else:
                     content = req.read().decode(charset)
-                    print('charset =', charset)
+                    #print('charset =', charset)
+                    Logger.logger.info('charset is %s', charset)
                 finally:
                     with open(file_path, 'w', encoding='utf-8') as f:
                         f.write(content)
-                    print('got', url)
+                    #print('got', url)
+                    Logger.logger.info('GOT %s', url)
 
                 return True
             else:
-                print(url, 'bad content-type:', content_type)
+                #print(url, 'bad content-type:', content_type)
+                Logger.logger.error('%s has a bad content-type: %s', url, content_type)
                 return False
 
 
@@ -87,16 +95,19 @@ def ungzip_file(gz_file):
             with gzip.open(gz_file, 'rb') as f:
                 content = f.read()
         except Exception as error:
-            print(error)
+            #print(error)
+            Logger.logger.error('could not decompress %s . output: %s', gz_file, error)
             return False
         else:
             try:
                 with open(out_xml_file, 'wb') as f2:
                     f2.write(content)
             except Exception as error:
-                print(error)
+                #print(error)
+                Logger.logger.info('could not open %s . output: %s', out_xml_file, error)
             else:
-                print(gz_file, 'was successfully ungzipped.')
+                #print(gz_file, 'was successfully ungzipped.')
+                Logger.logger.info('%s was succesfully decompressed.', gz_file)
                 return True
 
 
@@ -110,7 +121,7 @@ def download_html():
         page_id = url[3]
 
         path_to_file = make_path_to_file(current_url, site_name)
-        print('path to file in downloader', path_to_file)
+        #print('path to file in downloader', path_to_file)
 
         if re.search(r'[^\s]+robots\.txt$', current_url):
             if get_file(current_url, path_to_file):
